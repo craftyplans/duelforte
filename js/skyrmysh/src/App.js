eaa09baa3de9d25b7a78f15628f5f6d246b43e5d
sb <sbenthall@gmail.com>
2019-10-18 09:59:23 -0400

    render list of cards to DOM

--- js/skyrmysh/src/App.js
+++ js/skyrmysh/src/App.js
@@ -2,21 +2,35 @@ import React from 'react';
 import logo from './skyrmysh-logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="logo" alt="logo" />
-        
-          <div className="body">
-          <p>Hello World.</p>
-          </div>
-         
-          </header>
+const cards = [
+    "Quick Attack",
+    "Hard Attack",
+    "Precise Attack",
+    "Quick Defend",
+    "Hard Defend",
+    "Precise Defend",
+    "Move",
+    "Cast Spell",
+    "Focus",
+    "Other",
+    "Full"
+]
 
-      
-      </div>
-           
+//function cardDOM(card){
+//    return <div>{card}<div>
+//}
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="logo" alt="logo" />
+            </header>    
+            <div className="body">
+            <p>Select up to 2 cards.</p>
+            {cards.map(c => <div>{c}</div>)}
+            </div>
+        </div>
   );
 }
 
