3be68920bb79d4048ee5037ec0050718c7acf0d3
sb <sbenthall@gmail.com>
2019-10-18 09:27:56 -0400

    adding React app boilerplate

---
+++ js/skyrmysh/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './skyrmysh-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+        
+          <div className="body">
+          <p>Hello World.</p>
+          </div>
+         
+          </header>
+
+      
+      </div>
+           
+  );
+}
+
+export default App;
